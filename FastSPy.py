d98dda35f769e41325df79d9597a03758712076c
Niema Moshiri <niemamoshiri@gmail.com>
2019-04-17 13:15:58 -0700

    Added verbose mode

--- FastSPy.py
+++ FastSPy.py
@@ -28,18 +28,27 @@ def read_FASTA(stream):
 
 # run FastSPy
 if __name__ == "__main__":
-    import argparse; from gzip import open as gopen
+    import argparse; from gzip import open as gopen; from sys import stderr
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-r', '--reference', required=True, type=str, help="Reference Alignment (FASTA)")
     parser.add_argument('-e', '--estimated', required=True, type=str, help="Estimated Alignment (FASTA)")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Verbose")
     args = parser.parse_args()
+    global VERBOSE; VERBOSE = args.verbose
+    if VERBOSE:
+        print("Reading reference alignment from: %s" % args.reference, file=stderr); stderr.flush()
     if args.reference.lower().endswith('.gz'):
         ref_stream = gopen(args.reference)
     else:
         ref_stream = open(args.reference)
     ref = read_FASTA(ref_stream); ref_stream.close()
+    if VERBOSE:
+        print("Successfully read %d sequences from reference alignment" % len(ref), file=stderr)
+        print("Reading estimated alignment from: %s" % args.estimated, file=stderr); stderr.flush()
     if args.estimated.lower().endswith('.gz'):
         est_stream = gopen(args.estimated)
     else:
         est_stream = open(args.estimated)
     est = read_FASTA(est_stream); est_stream.close()
+    if VERBOSE:
+        print("Successfully read %d sequences from estimated alignment" % len(est), file=stderr); stderr.flush()
